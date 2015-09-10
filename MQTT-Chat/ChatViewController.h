7606d56a61a42144bb05feb197802c5670b543ae
Tibor Kertesz <hummerdikk@gmail.com>
2015-09-10 13:46:47 +0200

    Github add

---
+++ MQTT-Chat/ChatViewController.h
@@ -0,0 +1,16 @@
+//
+//  ChatViewController.h
+//  MQTT-Chat
+//
+//  Created by Kertész Tibor on 10/09/15.
+//  Copyright (c) 2015 Kertész Tibor. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ChatViewController : UIViewController
+
+@property (strong,nonatomic) NSString* userName;
+@property (strong,nonatomic) NSString* chatTopic;
+
+@end
---
+++ Podfile
@@ -0,0 +1,13 @@
+# Uncomment this line to define a global platform for your project
+# platform :ios, '6.0'
+
+target 'MQTT-Chat' do
+	pod 'MQTTClient', '0.2.6'
+	pod 'SlackTextViewController'
+end
+
+target 'MQTT-ChatTests' do
+	pod 'MQTTClient', '0.2.6'
+	pod 'SlackTextViewController'
+end
+
